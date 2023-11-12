891293219e9a3b6b96b9aa6ad13787c1ea2cefbe
sameerk007 <rokzsam007@gmail.com>
2023-11-12 11:17:48 +0000

    Single video file download fixed

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from pytube import Playlist
+from pytube import Playlist ,YouTube
 import os
 
 # Function to download a range of videos from a YouTube playlist with a specific resolution
@@ -8,16 +8,21 @@ def download_playlist_range(playlist_url, output_path, start_index, end_index, r
     total_videos = len(playlist.videos)
 
     # Validate the indices
-    if not 1 <= start_index <= total_videos or not 1 <= end_index <= total_videos or start_index > end_index:
-        st.warning("Invalid start or end index. Please enter valid indices within the range of the playlist.")
+    if end_index==0 or end_index>total_videos:
+        end_index = total_videos
+
+    if start_index > end_index:
+        st.warning("Invalid start or end index. Please enter valid indices within the range of the playlist.(1-Total_length)")
         return
+    
 
     # Create the output directory if it doesn't exist
     os.makedirs(output_path, exist_ok=True)
 
     st.write(f'Total Videos in Playlist: {total_videos}')
-    st.write(f'Downloading videos from index {start_index} to {end_index}')
 
+    st.write(f'Downloading videos from index {start_index} to {end_index}')
+    
     for index, video in enumerate(playlist.videos, start=1):
         if start_index <= index <= end_index:
             st.write(f'Downloading: {video.title}')
@@ -29,7 +34,11 @@ def download_playlist_range(playlist_url, output_path, start_index, end_index, r
                 selected_stream.download(output_path)
                 st.write(f'Download completed: {video.title}')
             else:
-                st.write(f'Video "{video.title}" does not have a {resolution} stream.')
+                st.write(f'Video "{video.title}" does not have a {resolution} stream. Trying for best resolution')
+                video.streams.get_highest_resolution().download(output_path)
+                st.write(f'Download completed: {video.title}')
+    
+
 
 def main():
     st.title("YouTube Playlist Downloader")
@@ -39,15 +48,30 @@ def main():
     desired_resolution = st.selectbox("Select Resolution", ["1080p", "720p", "480p"])
     download_path = st.text_input("Download Folder Path", "./downloads")
     start_index = st.number_input("Start Index", min_value=1, value=1)
-    end_index = st.number_input("End Index", min_value=1, value=1)
+    end_index = st.number_input("End Index", min_value=0, value=0)
 
     if st.button("Download"):
         if not playlist_url:
             st.warning("Please enter a YouTube Playlist URL.")
         else:
             st.write("Downloading...")
-            download_playlist_range(playlist_url, download_path, start_index, end_index, resolution=desired_resolution)
-            st.write("Download completed!")
+            try:
+                download_playlist_range(playlist_url, download_path, start_index, end_index, resolution=desired_resolution)
+            except:
+                video = YouTube(playlist_url)
+                st.write(f'Downloading: {video.title}')
+                # Filter streams by resolution
+                streams = video.streams.filter(res=desired_resolution)
+                if streams:
+                    # Choose the first stream (highest quality) with the specified resolution
+                    selected_stream = streams[0]
+                    selected_stream.download(download_path)
+                    st.write(f'Download completed: {video.title}')
+                else:
+                    st.write(f'Video "{video.title}" does not have a {desired_resolution} stream. Trying for best resolution')
+                    video.streams.get_highest_resolution().download(download_path)
+                    st.write(f'Download completed: {video.title}')
+                    st.write("Download completed!")
 
 if __name__ == "__main__":
     main()
